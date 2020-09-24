c8cb2569ef7ca85c436dbc6e7d12fcada11c4a0f
timothyd1 <52899859+timothyd1@users.noreply.github.com>
2020-09-23 21:08:47 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TechnicalAssignment3a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name below: ");
+
+            try
+            {
+                string name = Console.ReadLine();
+
+                Greeting_Statement(name);
+
+                Console.WriteLine(" ");
+                Console.WriteLine("Press any key to exit the program and try again...");
+                Console.ReadKey(true);
+            }
+
+            catch
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Please enter a name...");
+                Console.WriteLine(" ");
+                Console.WriteLine("Press any key to exit the program and try again...");
+                Console.ReadKey(true);
+            }
+        }
+
+        private static void Greeting_Statement(string name)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
